fcdd7e9b86b42f909e62bc954931fe24bf34e81e
Diogo Moreira Bispo <diogo.bispo@paulista.3con>
2020-04-18 14:39:30 -0300

    Navbar added and Dependencies installed
    Added Navbar with BMI and Pace Calculator links.
    Added dependencies:
    - React Bootstrap
    - React Redux
    - Redux

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,21 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import { Navbar, Nav } from "react-bootstrap";
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+
+const Header = () => (
+  <header>
+    <Navbar collapseOnSelect expand="lg" variant="light" bg="light">
+      <Navbar.Brand href="#home">Fitness Calculators</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Body Mass Index</Nav.Link>
+          <Nav.Link href="#pricing">Pace Calculator</Nav.Link>          
+        </Nav>        
+      </Navbar.Collapse>
+    </Navbar>
   </header>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
